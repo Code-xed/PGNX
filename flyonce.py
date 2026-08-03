9d39bac52f49cf7307a274d4663ffcf85c8e9411
Code-xed <ngudulu@gmail.com>
2026-08-04 02:48:28 +0530

    Update flyonce.py

--- flyonce.py
+++ flyonce.py
@@ -1,7 +1,6 @@
 import asyncio
 from playwright.async_api import async_playwright
 
-
 URL = "https://gamefactory.zone/chess/app#cses=nhfJ44RzW9J6iON3XnVBaw&uid=5776831017%40telegram&ctype=chess&uname=Uraraka+Deku+san&domain=telegram&sign=IO7f6HC0ED1wWtnwab3lfrvXraI3zKhVKzkgugfqeOcKz5qFhJsoGCm%2BywrkdiIOcugsXkg84VYKondnhcG2hfbJ2hW2d12lWA%2BHs%2FFY1Cy0rt4SHATFjImriaOTNCfwu7lb8pSNXGsGexebyZhAGEGitpowvIuftWUtp%2FZKNRA%3D&subj=BQAAAPLn1iD_____MC8DAEsSctc-QTn8&logout=false"
 
 
@@ -12,78 +11,66 @@ async def main():
             args=[
                 "--no-sandbox",
                 "--disable-dev-shm-usage",
-                "--disable-gpu",
-                "--no-zygote",
-                "--single-process",
             ],
         )
 
         page = await browser.new_page()
 
+        page.on("console", lambda msg: print(msg.text))
+
+        await page.add_init_script("""
+(() => {
+    const NativeWS = window.WebSocket;
+
+    window.WebSocket = function(...args) {
+        console.log("[WS OPEN]", args[0]);
+
+        const ws = new NativeWS(...args);
+
+        const send = ws.send.bind(ws);
+
+        ws.send = function(data) {
+            console.log("[WS SEND]", data);
+            return send(data);
+        };
+
+        ws.addEventListener("message", e => {
+            console.log("[WS RECV]", e.data);
+        });
+
+        ws.addEventListener("open", () => {
+            console.log("[WS CONNECTED]");
+        });
+
+        ws.addEventListener("close", () => {
+            console.log("[WS CLOSED]");
+        });
+
+        return ws;
+    };
+
+    window.WebSocket.prototype = NativeWS.prototype;
+})();
+""")
+
         page.on(
             "request",
-            lambda r: print(f"\n>>> REQUEST {r.method} {r.url}")
+            lambda r: print(f">>> {r.method} {r.url}")
         )
-
-        async def on_response(resp):
-            try:
-                print(f"\n<<< RESPONSE {resp.status} {resp.url}")
-
-                ct = resp.headers.get("content-type", "")
-
-                if any(
-                    x in ct
-                    for x in (
-                        "json",
-                        "javascript",
-                        "text",
-                        "xml",
-                    )
-                ):
-                    body = await resp.text()
-                    print(body[:5000])
-
-            except Exception as e:
-                print("Response error:", e)
 
         page.on(
             "response",
-            lambda r: asyncio.create_task(on_response(r))
+            lambda r: print(f"<<< {r.status} {r.url}")
         )
 
-        def on_websocket(ws):
-            print("\n==============================")
-            print("WEBSOCKET")
-            print(ws.url)
-            print("==============================")
-
-            ws.on(
-                "framesent",
-                lambda data: print(f"\n>> SENT\n{data}")
-            )
-
-            ws.on(
-                "framereceived",
-                lambda data: print(f"\n<< RECEIVED\n{data}")
-            )
-
-            ws.on(
-                "close",
-                lambda: print("\nWEBSOCKET CLOSED")
-            )
-
-        page.on("websocket", on_websocket)
-
         print("Opening...")
-        await page.goto(URL)
 
-        print("Title:", await page.title())
-        print("URL:", page.url)
+        await page.goto(URL, wait_until="networkidle")
 
-        # Give the websocket time to exchange messages
-        await page.wait_for_timeout(60000)
+        print(await page.title())
 
-        print("Finished sniffing.")
+        # Give the page plenty of time
+        await page.wait_for_timeout(120000)
 
         await browser.close()
 
